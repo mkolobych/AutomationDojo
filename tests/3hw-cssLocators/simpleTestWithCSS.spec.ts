78ec558be86d1442b586c69f41fca9a7f175db23
Maryan Kolobych <mkolobich@gmail.com>
2025-03-07 15:29:35 +0100

    added css selectors

--- tests/3hw-cssLocators/simpleTestWithCSS.spec.ts
+++ tests/3hw-cssLocators/simpleTestWithCSS.spec.ts
@@ -7,55 +7,55 @@ test.describe("Cart tests", () => {
     })
 
     test("Mocha should contain whipped cream, steamed milk, chocolate syrup, espresso", async ({ page }) => {
-        await expect(page.locator("//div[@data-test='Mocha']")).toBeVisible();
-        await expect(page.locator("//div[@data-test='Mocha']/div[1]")).toContainText("espresso");
-        await expect(page.locator("//div[@data-test='Mocha']/div[2]")).toContainText("chocolate syrup");
-        await expect(page.locator("//div[@data-test='Mocha']/div[3]")).toContainText("steamed milk");
-        await expect(page.locator("//div[@data-test='Mocha']/div[4]")).toContainText("whipped cream");
+        await expect(page.locator("[data-test='Mocha']")).toBeVisible();
+        await expect(page.locator("div[data-test='Mocha'] > div:nth-child(1)")).toContainText("espresso");
+        await expect(page.locator("div[data-test='Mocha'] > div:nth-child(2)")).toContainText("chocolate syrup");
+        await expect(page.locator("div[data-test='Mocha'] > div:nth-child(3)")).toContainText("steamed milk");
+        await expect(page.locator("div[data-test='Mocha'] > div:nth-child(4)")).toContainText("whipped cream");
     });
 
     test("'No coffee, go add some.' message is visible", async ({ page }) => {
-        await page.locator("//a[@aria-label='Cart page']").click();
+        await page.locator("a[aria-label='Cart page'], div[class='list']").click();
 
-        await expect(page.locator("//div[@class='list']")).toContainText("No coffee, go add some.");
-        await expect(page.locator("//button[@class='pay']")).not.toBeVisible();
-        await expect(page.locator("//*[@id='app']/div[2]/div/ul")).not.toBeVisible();
+        await expect(page.locator("div[class='list'] p")).toContainText("No coffee, go add some.");
+        await expect(page.locator(".pay")).not.toBeVisible();
+        await expect(page.locator("//*[@id='app']/div[2]/div/ul")).not.toBeVisible(); //який селектор тут краще обрати?
     });
 
     test("The extra cup of Mocha can be skipped", async ({ page }) => {
-        await page.locator("//ul[@data-v-a9662a08]/li[1]").click({ clickCount: 3 });
+        await page.locator("div[data-test='Mocha']").click({ clickCount: 3 });
 
-        await expect(page.locator("//div[@ class='promo']")).toBeVisible();
+        await expect(page.locator(".promo")).toBeVisible();
 
-        await page.locator("//div[@class='buttons']/button[2]").click();
+        await page.locator(".yes").click();
 
-        await expect(page.locator("//div[@ class='promo']")).not.toBeVisible();
-        await expect(page.locator('//a[@aria-label="Cart page"]')).toContainText("cart (3)")
+        await expect(page.locator(".promo")).not.toBeVisible();
+        await expect(page.locator('a[aria-label="Cart page"]')).toContainText("cart (3)")
     });
 
     test("Espresso Macchiato can be removed from the Cart", async ({ page }) => {
-        await page.locator("//ul[@data-v-a9662a08]/li[2]").click();
-        await page.locator("//a[@aria-label='Cart page']").click();
+        await page.locator("[data-test='Espresso_Macchiato']").click();
+        await page.locator("[aria-label='Cart page']").click();
 
-        await expect(page.locator("//div[contains(text(), 'Espresso Macchiato')]")).toBeVisible();
+        await expect(page.locator("//div[contains(text(), 'Espresso Macchiato')]")).toBeVisible();// чи доцільно використовувати сss?
 
-        await page.locator('//button[@class="delete"]').click();
+        await page.locator('.delete').click();
 
-        await expect(page.locator("//div[@class='list']")).toContainText("No coffee, go add some.");
-        await expect(page.locator("//button[@class='pay']")).not.toBeVisible();
+        await expect(page.locator(".list")).toContainText("No coffee, go add some.");
+        await expect(page.locator(".pay")).not.toBeVisible();
         await expect(page.locator("//*[@id='app']/div[2]/div/ul")).not.toBeVisible();
     });
 
     test("Cappuccino can be removed from the Cart via the minus button", async ({ page }) => {
-        await page.locator("//ul[@data-v-a9662a08]/li[3]").click();
-        await page.locator("//a[@aria-label='Cart page']").click();
+        await page.locator("div[data-test='Cappuccino']").click();
+        await page.locator("a[aria-label='Cart page']").click();
 
         await expect(page.locator('//div[contains(text(),"Cappuccino")]')).toBeVisible();
 
-        await page.locator("//li[@class='list-item']/div/div/button[2]").click();
+        await page.locator("li[class='list-item'] div div[class='unit-controller'] button[aria-label='Remove one Cappuccino']").click(); ////li[@class='list-item']/div/div/button[2]
 
-        await expect(page.locator("//div[@class='list']")).toContainText("No coffee, go add some.");
-        await expect(page.locator("//button[@class='pay']")).not.toBeVisible();
+        await expect(page.locator(".list")).toContainText("No coffee, go add some.");
+        await expect(page.locator(".pay'")).not.toBeVisible();
         await expect(page.locator("//*[@id='app']/div[2]/div/ul")).not.toBeVisible();
     });
 });
