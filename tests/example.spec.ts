2e1629c2958b8e43c39a4c554381b1cd20b94a2d
Maryan Kolobych <mkolobich@gmail.com>
2025-03-15 18:06:33 +0100

    practice with browser, context and page

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,4 +1,4 @@
-import { test, expect } from '@playwright/test';
+import { test, expect, chromium, webkit } from '@playwright/test';
 
 test('has title', async ({ page }) => {
   await page.goto('https://playwright.dev/');
@@ -16,3 +16,17 @@ test('get started link', async ({ page }) => {
   // Expects page to have a heading with the name of Installation.
   await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
 });
+
+test("test", async () => {
+  const browser = await chromium.launch({ headless: false }); // запускає бравзер Сromium (firefox, webkit..etc ); параметр, що вимикає безголовий режим (headless mode), тобто браузер відкриється з графічним інтерфейсом, як звичайний браузер.
+  // Якщо headless: true (за замовчуванням) — браузер працює у фоні без графічного інтерфейсу.
+  // Якщо headless: false — браузер відкривається з вікном, що корисно для налагодження тестів або автоматизації.
+
+  // const page = await browser.newPage(); // створює сторінку бравзера (кожна сторінка створюється в окремо)
+  const context = await browser.newContext(); // об'єднує сторінки в бравзерві (авторизація в одній педжі пошириться на інші пейджі, об'єднує сесії в одну)
+  const page = await context.newPage();
+  const page1 = await context.newPage();
+  const page2 = await context.newPage();
+
+  console.log("sssd")
+})
